840400ca0938ad40edb998fab1aec0924184bcb9
Voron <m.keda@internetdevels.com>
2017-06-20 14:07:09 +0300

    Installed mysql.connector.

--- models.py
+++ models.py
@@ -1,8 +1,16 @@
 from peewee import MySQLDatabase, Model, CharField, ForeignKeyField
 from playhouse.hybrid import hybrid_property
 
+from mysql.connector import MySQLConnection, Error
+
 # todo: don't commit this
 db = MySQLDatabase('test_task', user='test_admin', password='test_admin_pass')
+db_config = {
+    'user': 'test_admin',
+    'password': 'test_admin_pass',
+    'host': '127.0.0.1',
+    'database': 'test_task'
+}
 
 STATUSES = (
     ('0', 'Inactive'),
@@ -14,6 +22,26 @@ class BaseModel(Model):
     """BaseModel model"""
     class Meta:
         database = db
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            self._db_connection = MySQLConnection(**db_config)
+            self._db_cur = self._db_connection.cursor()
+            print(self._db_cur)
+        except Error as e:
+            print(e)
+
+    def __del__(self):
+        self._db_cur = self._db_connection.close()
+        self._db_connection.close()
+
+    def my_get_tables(self):
+        print('in')
+        try:
+            print(self._db_cur.execute("SHOW TABLES").commit())
+        except Error:
+            self._db_connection.rollback()
 
 
 class User(BaseModel):

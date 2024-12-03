935d981e7141a362606d9f5029158fc9ad569c92
Mykhailo Keda <mkeda@Mac-Studio.local>
2024-12-03 00:09:39 +0000

    Added Redis Session Interface.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sanic.log import logger
 from sanic.request import Request
 from sanic.response import HTTPResponse
 from sanic_jinja2 import SanicJinja2
-from sanic_session import Session, AIORedisSessionInterface
+from sanic_session import Session
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from template_tags import update_param
@@ -20,6 +20,44 @@ jinja.env.globals["update_param"] = update_param
 
 session = Session()
 
+class RedisSessionInterface(BaseSessionInterface):
+    def __init__(
+        self,
+        redis,
+        domain: str = None,
+        expiry: int = 2592000,
+        httponly: bool = True,
+        cookie_name: str = "session",
+        prefix: str = "session:",
+        sessioncookie: bool = False,
+        samesite: str = None,
+        session_name: str = "session",
+        secure: bool = False,
+    ):
+
+        self.redis = redis
+
+        super().__init__(
+            expiry=expiry,
+            prefix=prefix,
+            cookie_name=cookie_name,
+            domain=domain,
+            httponly=httponly,
+            sessioncookie=sessioncookie,
+            samesite=samesite,
+            session_name=session_name,
+            secure=secure,
+        )
+
+    async def _get_value(self, prefix, sid):
+        return await self.redis.get(self.prefix + sid)
+
+    async def _delete_key(self, key):
+        await self.redis.delete(key)
+
+    async def _set_value(self, key, data):
+        await self.redis.setex(key, self.expiry, data)
+
 
 @app.listener("before_server_start")
 async def before_server_start(_app, _):
@@ -31,7 +69,7 @@ async def before_server_start(_app, _):
     # init extensions fabrics
     session.init_app(
         _app,
-        interface=AIORedisSessionInterface(
+        interface=RedisSessionInterface(
             _app.ctx.redis,
             samesite="Strict",
             cookie_name="session" if _app.config["DEBUG"] else "__Host-session",
